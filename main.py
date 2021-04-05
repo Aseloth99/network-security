c854c8b759b4ff01df21efef911767472a372351
Mustafa Açık <mustafaacik92@hotmail.com>
2021-04-06 00:27:59 +0300

    link eklendi

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #Only works in @gmail.com
-
+#https://app.diagrams.net/?src=about#HAseloth99%2Fnetwork-security%2Fmain%2FdatabaseMA  #diagram link
 #pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 
 import FBConf as fb
